a6b11fa909b9edf99d065247d2f251c850c48f95
ThienNguyen <thien.nguyen@sotatek.com>
2021-12-19 16:15:08 +0700

    udpate pool id

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,6 @@ export const clockSysvarAccount: PublicKey = new PublicKey(
   'SysvarC1ock11111111111111111111111111111111',
 );
 
-export const POOL_PROGRAM_ID = 'FVpKG2sKVC48gDnPf1gS1ugiemBivruPkRFqpgtBp23A';
+export const POOL_PROGRAM_ID = '9MSr4CEgjqPKi5X5oTShJKfitGpWmy9DwvWdQgtV1WPn';
 
-export const CURRENT_STAKE_PROGRAM_ID = 'FVpKG2sKVC48gDnPf1gS1ugiemBivruPkRFqpgtBp23A';
+export const CURRENT_STAKE_PROGRAM_ID = '9MSr4CEgjqPKi5X5oTShJKfitGpWmy9DwvWdQgtV1WPn';
