518cbf6d4901561e6900030f69927703d453bb1c
ThienNguyen <thien.nguyen@sotatek.com>
2022-01-06 22:21:04 +0700

    update program id

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,7 @@ export const clockSysvarAccount: PublicKey = new PublicKey(
   'SysvarC1ock11111111111111111111111111111111',
 );
 
-export const POOL_PROGRAM_ID = 'FJtQmNFFvu4vBD1mxB6HCUyRAgTsCQRKh6Xc8kRdwEc1';
+export const POOL_PROGRAM_ID = '6vT2MD8vbhjqGMMxYnU2rHB1zEj3KAmxrvpts1zruVdi';
+// export const POOL_PROGRAM_ID = 'FJtQmNFFvu4vBD1mxB6HCUyRAgTsCQRKh6Xc8kRdwEc1';
 
-export const CURRENT_STAKE_PROGRAM_ID = 'FJtQmNFFvu4vBD1mxB6HCUyRAgTsCQRKh6Xc8kRdwEc1';
+export const CURRENT_STAKE_PROGRAM_ID = '6vT2MD8vbhjqGMMxYnU2rHB1zEj3KAmxrvpts1zruVdi';
